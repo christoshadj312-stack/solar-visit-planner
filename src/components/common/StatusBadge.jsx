46827304ca529321ec0f310460da4f76129f19b6
christoshadj312-stack <christoshadj312@gmail.com>
2026-08-30 14:29:49 +0300

    Add sanitized src/components/common/StatusBadge.jsx

---
+++ src/components/common/StatusBadge.jsx
@@ -0,0 +1,12 @@
+﻿import { STATUS_TONE } from "../../config/statuses.js";
+import { useTranslation } from "../../i18n/index.js";
+
+export function StatusBadge({ status }) {
+  const { t } = useTranslation();
+
+  return (
+    <span className="status-badge" data-tone={STATUS_TONE[status] || "green"}>
+      {t(`status.${status}`)}
+    </span>
+  );
+}
